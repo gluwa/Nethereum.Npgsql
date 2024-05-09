9fefc5e64e2939d2fafaf71e7b43fa35649de02b
dlebee <36390643+dlebee@users.noreply.github.com>
2024-05-09 17:22:03 -0400

    Feature/migration support (#7)
    * initial migraiton works.
    
    * migrations is now possible
    
    * getting method once and static instead of each time.

---
+++ Demo.Api/Migrations/20240418184813_initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Nethereum.Util;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Demo.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "numbers",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    large_number = table.Column<BigDecimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_numbers", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "numbers");
+        }
+    }
+}
--- Nethereum.Npgsql.EntityFrameworkCore/NpgsqlBigDecimalTypeMapping.cs
+++ Nethereum.Npgsql.EntityFrameworkCore/NpgsqlBigDecimalTypeMapping.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Json;
 using Nethereum.Util;
@@ -14,6 +16,8 @@ namespace Nethereum.Npgsql.EntityFrameworkCore;
 /// </summary>
 public class NpgsqlBigDecimalTypeMapping : NpgsqlTypeMapping
 {
+
+    public static MethodInfo BigDecimalParseMethod = typeof(BigDecimal).GetMethod(nameof(BigDecimal.Parse), BindingFlags.Static | BindingFlags.Public)!;
     private const string DecimalFormatConst = "{0:0.0###########################}";
 
     /// <summary>
@@ -82,4 +86,15 @@ public class NpgsqlBigDecimalTypeMapping : NpgsqlTypeMapping
     /// </summary>
     protected override string SqlLiteralFormatString
         => DecimalFormatConst;
+
+    internal static MethodCallExpression ConstantCall(MethodInfo method, params object[] parameters)
+        => Expression.Call(method, parameters.Select(p => Expression.Constant(p)).ToArray());
+
+    public override Expression GenerateCodeLiteral(object value)
+    {
+        var bigDecimalValue = (BigDecimal)value;
+        var stringValue = $"{bigDecimalValue}";
+        var expression = ConstantCall(BigDecimalParseMethod, stringValue);
+        return expression;
+    }
 }
